d7ded9085adf471735826f4c219f2006581af212
Svetlana Davidchuk <stdavidchuk@gmail.com>
2022-07-17 20:26:38 -0400

    bio page complete - comments section added

---
+++ scripts/index-page.js
@@ -0,0 +1,11 @@
+// // Form for the name
+
+// const itemForm = document.getElementById("comment-form");
+// const dynamicContent = document.getElementById("new-name");
+
+// itemForm.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   const itemInputVal = event.target.fullName.value; 
+//   listElement.innerText = itemInputVal;
+//   dynamicContent.appendChild(listElement);
+// });
